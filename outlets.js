1dd342ce406a63e32a21d5965333ff5b29960ed9
le-pain-531 <qycchn@gmail.com>
2026-07-16 17:45:11 +0800

    Add outlet data for various branches in Singapore

---
+++ outlets.js
@@ -0,0 +1,438 @@
+// mapsQuery is optional — defaults to "Wonder Wash {name} Singapore".
+// Only set it when the name alone won't find the right place on Google Maps.
+// loyaltyCard: true — add this field to enrol an outlet in the loyalty programme.
+
+export const outlets = [
+  {
+    name: "Jalan Bukit Merah Branch",
+    region: "South",
+    address: "Block 111 Jalan Bukit Merah #01-1696 Singapore 160111",
+    lat: 1.279773996808,
+    lng: 103.826323078864,
+    loyaltyCard: true
+  },
+  {
+    name: "Telok Blangah Branch",
+    region: "South",
+    address: "Block 78A Telok Blangah St 32 #01-13 Singapore 102078",
+    lat: 1.273387531412,
+    lng: 103.808980585918
+  },
+  {
+    name: "Mei Ling Street Branch",
+    region: "South",
+    address: "Block 157 Mei Ling Street #01-68 Singapore 140157",
+    lat: 1.293111951693,
+    lng: 103.803730245588
+  },
+  {
+    name: "Jalan Kukoh / Outram Branch",
+    region: "South",
+    address: "Block 2 Jalan Kukoh #01-141 Singapore 163002",
+    lat: 1.288579689008,
+    lng: 103.839614514187
+  },
+  {
+    name: "Sims Drive Branch",
+    region: "South",
+    address: "Block 45 Sims Drive #01-170 Singapore 380045",
+    lat: 1.317337202522,
+    lng: 103.878442674819
+  },
+  {
+    name: "Kim Tian Branch",
+    region: "South",
+    address: "Block 129 Kim Tian Road #01-129 Singapore 160129",
+    lat: 1.281153029427,
+    lng: 103.828856173823
+  },
+  {
+    name: "Everton Park Branch",
+    region: "South",
+    address: "Block 3 Everton Park #01-73 Singapore 080003",
+    lat: 1.277217306791,
+    lng: 103.838703988045
+  },
+  {
+    name: "Depot Road Branch",
+    region: "South",
+    address: "Block 108 Depot Road #01-13 Depot Height Shopping Centre Singapore 100108",
+    lat: 1.280844701119,
+    lng: 103.810292605222
+  },
+  {
+    name: "Chinatown Branch (Hong Lim Complex)",
+    region: "South",
+    address: "Block 531 Upper Cross Street #01-48 Singapore 050531 (2 minutes walk from Chinatown MRT next to Yan Palace Restaurant)",
+    lat: 1.284392383548,
+    lng: 103.846101436692
+  },
+  {
+    name: "Telok Blangah Heights Branch",
+    region: "South",
+    address: "Block 88A Telok Blangah Heights #01-02 Singapore 101088",
+    lat: 1.277293288744,
+    lng: 103.80773989661
+  },
+  {
+    name: "28 Jalan Bukit Merah Branch",
+    region: "South",
+    address: "Block 28 Jalan Bukit Merah #01-4770 Singapore 152028",
+    lat: 1.286358526576,
+    lng: 103.809405296952
+  },
+  {
+    name: "Pasir Ris Branch",
+    region: "East",
+    address: "Block 625 Elias Rd #01-328 Singapore 510625",
+    lat: 1.378602418759,
+    lng: 103.941978171441
+  },
+  {
+    name: "Chai Chee Branch",
+    region: "East",
+    address: "Block 26A Chai Chee Road #01-395 Singapore 461026",
+    lat: 1.323700491955,
+    lng: 103.923497164586
+  },
+  {
+    name: "Tampines Branch (New)",
+    region: "East",
+    address: "Block 139, #01-64 Tampines Street 11 Singapore 521139",
+    lat: 1.346160723523,
+    lng: 103.944509254743,
+    loyaltyCard: true
+  },
+  {
+    name: "Kallang / Upper Boon Keng Branch",
+    region: "East",
+    address: "Block 5 Upper Boon Keng Road #01-19 Singapore 380005",
+    lat: 1.312654484675,
+    lng: 103.872541814983
+  },
+  {
+    name: "Bedok Reservoir Branch",
+    region: "East",
+    address: "Block 703 Bedok Reservoir Road #01-3514 Singapore 470703",
+    lat: 1.337165055488,
+    lng: 103.91989303004
+  },
+  {
+    name: "New Upper Changi Branch",
+    region: "East",
+    address: "Block 56 New Upper Changi Road #01-1292 Singapore 461056",
+    lat: 1.324993777706,
+    lng: 103.941082565086
+  },
+  {
+    name: "Bedok North Ave 1 Branch",
+    region: "East",
+    address: "Block 551 Bedok North Avenue 1 #01-540 Singapore 460551",
+    lat: 1.332268515291,
+    lng: 103.92703659084
+  },
+  {
+    name: "Pasir Ris St 51 Branch",
+    region: "East",
+    address: "Block 527D Pasir Ris St 51 #01-02 Singapore 514527",
+    lat: 1.369686253075,
+    lng: 103.947922449535
+  },
+  {
+    name: "Tampines Ave 8 Branch",
+    region: "East",
+    address: "Block 878C Tampines Ave 8 #01-67 Singapore 523878",
+    lat: 1.351264129091,
+    lng: 103.929363826257
+  },
+  {
+    name: "Tampines St 42 Branch",
+    region: "East",
+    address: "Block 445 Tampines St 42 #01-04 Singapore 520445",
+    lat: 1.358302413187,
+    lng: 103.95065838655
+  },
+  {
+    name: "Ang Mo Kio Ave 10 Branch",
+    region: "Central",
+    address: "Block 555 Ang Mo Kio Avenue 10 #01-1946 Singapore 560555",
+    lat: 1.371196672028,
+    lng: 103.857023978539
+  },
+  {
+    name: "Ang Mo Kio St 31 Branch",
+    region: "Central",
+    address: "Block 316B Ang Mo Kio Street 31 #01-02 Singapore 563316",
+    lat: 1.364625385667,
+    lng: 103.847677066851
+  },
+  {
+    name: "Thomson Road Branch",
+    region: "Central",
+    address: "Block 1 Thomson Road #01-334 Singapore 300001",
+    lat: 1.32559593363,
+    lng: 103.842571649627
+  },
+  {
+    name: "Ang Mo Kio Ave 4 Branch",
+    region: "Central",
+    address: "Block 114 Ang Mo Kio Avenue 4 #01-357 Singapore 560114",
+    lat: 1.372614009945,
+    lng: 103.836201989947
+  },
+  {
+    name: "Toa Payoh Blk 211 Branch",
+    region: "Central",
+    address: "Block 211 Lorong 8 Toa Payoh #01-07 Singapore 310211",
+    lat: 1.340421802057,
+    lng: 103.853768866441
+  },
+  {
+    name: "Crawford Lane Branch",
+    region: "Central",
+    address: "Block 462 Crawford Lane #01-27 Singapore 190462",
+    lat: 1.304586190407,
+    lng: 103.860840761969
+  },
+  {
+    name: "Bishan Branch",
+    region: "Central",
+    address: "Block 106 Bishan Street 12 #01-220 Singapore 570106",
+    lat: 1.346100709097,
+    lng: 103.847847219486
+  },
+  {
+    name: "Ang Mo Kio Street 21 Branch",
+    region: "Central",
+    address: "Block 260 Ang Mo Kio Street 21 #01-02 Singapore 560260",
+    lat: 1.368916165721,
+    lng: 103.834499541593
+  },
+  {
+    name: "Ang Mo Kio Ave 1 Branch",
+    region: "Central",
+    address: "Block 226E Ang Mo Kio Avenue 1 #01-701 Singapore 565226",
+    lat: 1.366492454599,
+    lng: 103.839794892743
+  },
+  {
+    name: "Sin Ming Branch",
+    region: "Central",
+    address: "Block 22 Sin Ming Road #01-218 Singapore 570022",
+    lat: 1.357284431046,
+    lng: 103.839396668523
+  },
+  {
+    name: "Toa Payoh Blk 116 Branch",
+    region: "Central",
+    address: "Block 116 Toa Payoh Lorong 2 #01-156 Singapore 310116",
+    lat: 1.340472889757,
+    lng: 103.846142508018
+  },
+  {
+    name: "Toa Payoh Lorong 4 Branch",
+    region: "Central",
+    address: "Block 70 Toa Payoh Lorong 4 Singapore",
+    mapsQuery: "Wonder Wash Block 70 Toa Payoh Lorong 4 Singapore",
+    lat: 1.33440,
+    lng: 103.85242,
+    loyaltyCard: true
+  },
+  {
+    name: "Boon Tiong Road Branch",
+    region: "Central",
+    address: "Block 11A Boon Tiong Road #02-14 Singapore 161011",
+    lat: 1.286365776922,
+    lng: 103.828789872969
+  },
+  {
+    name: "Yishun Ave 11 Branch",
+    region: "North",
+    address: "Block 418, #01-419 Yishun Avenue 11 Singapore 760418",
+    lat: 1.424899316125,
+    lng: 103.847249757427,
+    loyaltyCard: true
+  },
+  {
+    name: "Woodlands Branch",
+    region: "North",
+    address: "Block 849 Woodlands Street 82 #01-207 Singapore 731849",
+    lat: 1.441310579628,
+    lng: 103.792065570034
+  },
+  {
+    name: "Hougang 212 Branch",
+    region: "North",
+    address: "Block 212 Hougang Street 21 #01-319 Singapore 530212",
+    lat: 1.359520890452,
+    lng: 103.888340026088
+  },
+  {
+    name: "Punggol Branch",
+    region: "North",
+    address: "Block 308C Punggol Walk #01-324 Singapore 823308",
+    lat: 1.408006931379,
+    lng: 103.899074394209
+  },
+  {
+    name: "Canberra Walk Branch",
+    region: "North",
+    address: "Block 115 Canberra Walk #01-03 Singapore 750115",
+    lat: 1.446081894192,
+    lng: 103.828739833007
+  },
+  {
+    name: "Compassvale Crescent Branch",
+    region: "North",
+    address: "Block 289C Compassvale Crescent #01-02 Singapore 543289",
+    lat: 1.397454995381,
+    lng: 103.896824523759
+  },
+  {
+    name: "Compassvale Street Branch",
+    region: "North",
+    address: "Block 263 Compassvale Street #01-08 Singapore 540263",
+    lat: 1.394848181584,
+    lng: 103.896042240735
+  },
+  {
+    name: "Yishun St 72 Branch",
+    region: "North",
+    address: "Block 737 Yishun St 72 #01-K1 Singapore 760737",
+    lat: 1.428975100363,
+    lng: 103.832444550391
+  },
+  {
+    name: "Anchorvale Branch",
+    region: "North",
+    address: "Block 351 Anchorvale Road #01-03 Singapore 540351",
+    lat: 1.393327871986,
+    lng: 103.886617628805
+  },
+  {
+    name: "Yishun Ave 6 Branch",
+    region: "North",
+    address: "Block 461 Yishun Ave 6 #01-09 Singapore 760461",
+    lat: 1.428760942737,
+    lng: 103.84975996979
+  },
+  {
+    name: "Hougang Ave 8 Branch",
+    region: "North",
+    address: "Block 506 Hougang Ave 8 #01-672 Singapore 530506",
+    lat: 1.372765722392,
+    lng: 103.887618847139
+  },
+  {
+    name: "Yishun Ave 4 Branch",
+    region: "North",
+    address: "Block 664 Yishun Avenue 4 #01-277 Singapore 760664",
+    lat: 1.420199876381,
+    lng: 103.84107464172
+  },
+  {
+    name: "Woodlands Blk 304 Branch",
+    region: "North",
+    address: "Block 304 Woodlands Street 31 #01-119 Singapore 730304",
+    lat: 1.429892541575,
+    lng: 103.773853957548
+  },
+  {
+    name: "Serangoon North Branch",
+    region: "North",
+    address: "Block 154 Serangoon North Avenue 1 #01-434 Singapore 550154",
+    lat: 1.370129823391,
+    lng: 103.873115556256
+  },
+  {
+    name: "Bukit Batok West Branch",
+    region: "West",
+    address: "Block 177 Bukit Batok West Ave 8 #01-251 Singapore 650177",
+    lat: 1.346344077655,
+    lng: 103.741742048292
+  },
+  {
+    name: "Jurong West Branch",
+    region: "West",
+    address: "Block 498 Jurong West St 41 #01-452 Singapore 640498",
+    lat: 1.348333354736,
+    lng: 103.724307354411
+  },
+  {
+    name: "Boon Lay Branch",
+    region: "West",
+    address: "Block 221 Boon Lay Place #01-148 Singapore 640221",
+    lat: 1.346335528114,
+    lng: 103.71243036525
+  },
+  {
+    name: "Clementi Ave 3 Branch",
+    region: "West",
+    address: "Block  431 Clementi Ave 3 #01-346 Singapore 120431",
+    lat: 1.313774390758,
+    lng: 103.762535203138
+  },
+  {
+    name: "Teban Gardens Branch",
+    region: "West",
+    address: "Block  61 Teban Gardens Road #02-01 Singapore 600061",
+    lat: 1.322136345004,
+    lng: 103.738292811317
+  },
+  {
+    name: "Bukit Panjang Outlet",
+    region: "West",
+    address: "Green Ridge Shopping Centre",
+    lat: 1.3853278,
+    lng: 103.7662528
+  },
+  {
+    name: "Choa Chu Kang Branch",
+    region: "West",
+    address: "Block 810 Choa Chu Kang Avenue 7 Singapore 680810",
+    lat: 1.374054607644,
+    lng: 103.746160903827
+  },
+  {
+    name: "Jurong West St 91 Branch",
+    region: "West",
+    address: "Block 909 Jurong West Street 91 #01-259 Singapore 640909",
+    lat: 1.341175167515,
+    lng: 103.6864521651
+  },
+  {
+    name: "Jurong West Ave 1 Branch",
+    region: "West",
+    address: "Block 443 Jurong West Ave 1 #01-K1 Singapore 640443",
+    lat: 1.351802540641,
+    lng: 103.720394179095
+  },
+  {
+    name: "NEWest Branch",
+    region: "West",
+    address: "No 1, West Coast Drive #01-27 NEWEST Singapore 128020",
+    lat: 1.316413223425,
+    lng: 103.757451243586
+  },
+  {
+    name: "Taman Jurong Branch",
+    region: "West",
+    address: "399 Yung Sheng Road #01-03 Taman Jurong Shopping Centre Singapore 610399",
+    lat: 1.335247653227,
+    lng: 103.719899060074
+  },
+  {
+    name: "Senja Road Branch",
+    region: "West",
+    address: "Block 628 Senja Road #01-05 Singapore 670628",
+    lat: 1.3851286943,
+    lng: 103.76041064214
+  },
+  {
+    name: "491 Jurong West Ave 1 Branch",
+    region: "West",
+    address: "Block 491 Jurong West Avenue 1 #01-161 Singapore 640491",
+    lat: 1.349586574989,
+    lng: 103.726315323136
+  }
+];
